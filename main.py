847cbdd9755f4fd5ca0d0301653d43163ed1a4a8
Chris Cooley <ccooley@bonadmin.com>
2024-04-19 08:42:26 -0600

    Feat: Add requests and functionality for workouts

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import json
+
 from fastapi import FastAPI
 
 from models import Workout
@@ -5,21 +7,33 @@ from models import Workout
 
 app = FastAPI()
 
+with open("workout.json", "r") as f:
+    data = json.load(f)
+
 workouts: list[Workout] = []
+
+for workout in data:
+    workouts.append(Workout(**workout))
 
 
 @app.get("/workouts")
-async def get_workouts() -> None:
+async def get_workouts() -> list[Workout]:
     return workouts
 
 @app.post("/workouts")
-async def create_workouts() -> None:
-    pass
+async def create_workouts(workout: Workout) -> None:
+    workouts.append(workout)
 
-@app.put("/workouts")
-async def update_workouts() -> None:
-    pass
+@app.put("/workouts{workout_id}")
+async def update_workouts(workout_id: int, updated_workout: Workout) -> None:
+    for i, workout in enumerate(workouts):
+        if workout.id == workout_id:
+            workouts[i] = updated_workout
+            return
 
-@app.delete("/workouts")
-async def delete_workouts() -> None:
-    pass
+@app.delete("/workouts{workout_id}")
+async def delete_workouts(workout_id: int) -> None:
+    for i, workout in enumerate(workouts):
+        if workout.id == workout_id:
+            workouts.pop(i)
+            return
--- models.py
+++ models.py
@@ -2,7 +2,8 @@ from pydantic import BaseModel
 
 
 class Workout(BaseModel):
-    exercise: str
+    id: int
+    name: str
     set: int
     rep: int
     weight: int
